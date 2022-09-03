9e4d58f529805d0a28e6cb3619d13322441b4f8d
mariianazarova <mariianazarova7@gmail.com>
2022-09-03 19:50:47 +0300

    add hm2

---
+++ hw2/script-1.js
@@ -0,0 +1,19 @@
+let myName = "Mariia";
+let mySurmame = "Nazarova";
+let myCity = "Bilgorod-Dnistrovskyi";
+let a = (2 + 3 + 4 + 6) / 4;
+let r = 4;
+const P = 3.14;
+let l = 2 * P * r;
+let S = P * r * r;
+
+/*let my-Name;*/
+/*let @myName;*/
+/*let .myName;*/
+
+console.log(myName); //Виводимо моє ім'я
+console.log(mySurmame); /*Виводимо моє прізвище*/
+console.log(myCity);
+console.log(a);
+console.log(l);
+console.log(S);
