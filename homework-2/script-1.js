e3476eddfc61834cbfe5fecea13a4403657dc56d
mariianazarova <mariianazarova7@gmail.com>
2022-10-09 22:39:46 +0300

    add changing

--- homework-2/script-1.js
+++ homework-2/script-1.js
@@ -1,18 +1,29 @@
-let myName = "Mariia";
-let mySurmame = "Nazarova";
-let myCity = "Bilgorod-Dnistrovskyi";
-let a = (2 + 3 + 4 + 6) / 4;
-let r = 4;
-let l = 2 * Math.PI * r;
-let S = Math.PI * Math.pow(r, 2);
+//створити змінну myName, та ініціалізувати її Вашим ім'ям
+//створити змінну mySurmame, та ініціалізувати її Вашим прізвищем
+//створити змінну myСity, та ініціалізувати її Вашим рідним містом
+const myName = "Mariia";
+const mySurmame = "Nazarova";
+const myCity = "Bilgorod-Dnistrovskyi";
 
+//створити змінну в яку зберегти середнє арифметичного 4 будь-яких чисел
+const arithmeticalMean = (2 + 3 + 4 + 6) / 4;
+
+//створити змінну та записати в неї радіус уявного кола
+//створити змінні для довжини та площі
+//обчислити їх та зберегти в них результат 
+const radiusCircle = 4;
+let lengthCircle = 2 * Math.PI * radiusCircle;
+let squareCircle = Math.PI * Math.pow(r, 2);
+
+//cтворити 3 неправильних змінних (закоментувати);
 /*let my-Name;*/
 /*let @myName;*/
 /*let .myName;*/
 
+//Результат всіх операцій вище виводити через console.log
 console.log(myName); //Виводимо моє ім'я
 console.log(mySurmame); /*Виводимо моє прізвище*/
 console.log(myCity);
-console.log(a);
-console.log(l);
-console.log(S);
+console.log(arithmeticalMean);
+console.log(lengthCircle);
+console.log(squareCircle);
